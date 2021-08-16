a17db79b1cdcd85bb4c559a1c0633f4b5d1942dd
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-08-16 11:40:43 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void selection_sort(int arr[],int n){
     for(int i=0;i<n-1;i++){
-        for(j=k=i;j<n-1;j++){
+        for(j=k=i;j<n;j++){
             if(arr[j]<arr[k]){
                 k=j; 
 
